aacefd7ce777bd46013365531709db1bdb6df5b7
Johannes Hertenstein <git@j6s.dev>
2021-05-14 11:34:29 +0200

    Revert "Add additional spaces to help with hand injury"
    This reverts commit 81a9f2a0d9c0bb29803122357291d31588748ddf.

--- keymap.c
+++ keymap.c
@@ -55,15 +55,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
      * | LCtrl  | lWin   | lAlt   | Space   | Space  | Space  | left  | down   | right | Enter   | [L1]   | rAlt   | [      | ]      | rCtrl  |
      * '--------------------------------------------------------------------------------------------------------------------------------------'
-     *
-     * NOTE: Temporarily, the whole middle lower row is spaces to help with an injury
      */
     [L0] = LAYOUT_ortho_5x15(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQUAL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F5,   _______, KC_F8,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_F6,   _______, KC_F7,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    _______, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LWIN, KC_LALT, KC_SPC, KC_SPC,  KC_SPC,  KC_SPC,   KC_SPC,  KC_SPC,  KC_ENT,  MO(L1),  KC_RALT, KC_LBRC, KC_RBRC, KC_RCTL
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_UP,   KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LWIN, KC_LALT, KC_SPC, KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_ENT,  MO(L1),  KC_RALT, KC_LBRC, KC_RBRC, KC_RCTL
     ),
 
     /*
